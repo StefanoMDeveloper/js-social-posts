930ca71c2d8a9dcf78407a28ac51e9a7d438a7ab
StefanoMDeveloper <stenmarti02@gmail.com>
2022-01-11 18:08:27 +0100

    Milestone 3, finito

--- main.js
+++ main.js
@@ -55,3 +55,71 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let idPostPiaciuti = [];
+
+
+let container = document.getElementById("container");
+for (let i = 0; i < posts.length; i++) {
+    container.innerHTML +=`
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[i].author.image}" alt="${posts[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${dataMese(posts[i].created)}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[i].content}</div>
+            <div class="post__image">
+                <img src="${posts[i].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${posts[i].id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${posts[i].id}" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+    
+}
+
+
+let bottoni = document.querySelectorAll(".like-button");
+for (let i = 0; i < bottoni.length; i++) {
+    
+    bottoni[i].addEventListener("click", function(event){
+        event.preventDefault(); //impedisce al link cliccato di aprire la pagina (in questo modo non torna all'inizio dell'html ogni volta che clicco).
+        
+        bottoni[i].classList.add("like-button--liked");
+        
+        posts[i].likes += 1;
+
+        aggiornaLike = document.getElementById("like-counter-" + posts[i].id);
+        aggiornaLike.innerHTML = posts[i].likes;
+        idPostPiaciuti.push(posts[i].id);
+        console.log(idPostPiaciuti);
+    });
+    
+}
+
+
+
+function dataMese(data) {
+    const elements = data.split('-');
+    console.log(elements);
+    let nuovaData = elements[2] + "-" + elements[1] + '-' + elements[0];
+    return nuovaData;
+}
